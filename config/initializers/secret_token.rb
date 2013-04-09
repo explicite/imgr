cc588357655714b0d32dc42e5cb5fdd418133a09
Jan Paw <explicite@windowslive.com>
2013-04-09 18:55:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imgr::Application.config.secret_token = '1919d1b0dbd106a8e2d621a2ebba32eec3fda1b824752b6a332aa1889df609d1e3c29aab20470c76f929edad1b88c01fc9f9b89c38263e4c0908658243eab5cd'
