8382fcc5271060693df99d37f3712669adf60553
Jan Paw <explicite@windowslive.com>
2013-04-09 20:10:48 +0200

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Imgr::Application.routes.draw do
+  get "static_pages/help"
+
+  get "static_pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
