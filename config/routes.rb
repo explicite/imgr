37f567e32661517734012798d1b8407ca373c0a5
Jan Paw <explicite@windowslive.com>
2013-04-09 21:35:30 +0200

    3.3.2 Passing Title Tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Imgr::Application.routes.draw do
 
   get "static_pages/home"
 
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
