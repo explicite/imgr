9e9a1f78d4c734f303a0b485cdcefac63f1b4c62
Jan Paw <explicite@windowslive.com>
2013-04-12 22:46:18 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,17 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have the h1 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
+      page.should have_selector('h1', :text => 'Imgr App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
