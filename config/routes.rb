f7d83661444a205b0d31cec2e279a491efc4365e
Jan Paw <explicite@windowslive.com>
2013-04-22 16:22:32 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Imgr::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
+
+  match '/signin', to: 'sessions#new'
+
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,17 +21,22 @@ describe "User pages" do
   describe "signup" do
   	before { visit signup_path }
 
-  	let(:submint) { "Create mu account" }
+  	let(:submit) { "Create my account" }
   	describe "with invalid information" do
   	  it "should not create a user" do
   		expect { click_button submit }.not_to change(User, :count)
   	  end
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error')}
+      end
   	end
 
   	describe "with valid information" do
  			before do
- 				fill_in "Name", 				with: "Example User"
- 				fill_in "Email", 				with: "user@example.com"
+ 				fill_in "Name", 				with: "Test User"
+ 				fill_in "Email", 				with: "test@user.com"
  				fill_in "Password", 		with: "foobar"
  				fill_in "Confirmation",	with: "foobar"
  			end 	  
@@ -39,6 +44,10 @@ describe "User pages" do
  			it "should create a user" do
  				expect { click_button submit }.to change(User, :count).by(1)
  			end
+
+      describe "after saving the user" do
+        it { should have_link('Sign out') }
+      end
   	end
   end
 end
