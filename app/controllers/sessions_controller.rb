1b3600b13458326d389c57a6c3593a3d8c252a7f
Jan Paw <explicite@windowslive.com>
2013-06-17 18:47:45 +0200

    password change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email])
-		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user
-		else
-			flash.now[:error] = 'Invalid email/password combination'
-			render 'new'
-		end
+		user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
 	end
 
 	def from_facebook
@@ -30,8 +30,6 @@ class SessionsController < ApplicationController
 		  
 		sign_in user
 		redirect_to user
-		
-		
 		
 	end
 
